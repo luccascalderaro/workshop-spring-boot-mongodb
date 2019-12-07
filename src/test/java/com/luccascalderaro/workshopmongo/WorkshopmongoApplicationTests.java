b53b1c063f06e65b939acb33513fca581c3f1626
luccascalderaro <luccashmc@hotmail.com>
2019-12-07 19:49:05 -0300

    Projeto Criado

---
+++ src/test/java/com/luccascalderaro/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luccascalderaro.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
